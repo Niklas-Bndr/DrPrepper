82c4f13ef1cc5b54bba77f853ef6353a9a311475
Niklas B <niklas.bender193@gmail.com>
2020-02-23 23:07:52 +0100

    install slf4j logging in kafka-listener method

--- src/main/java/de/thbingen/vsys/DrPrepper/DrPrepperApplication.java
+++ src/main/java/de/thbingen/vsys/DrPrepper/DrPrepperApplication.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.thbingen.vsys.DrPrepper.config.DnaSequenceProducer;
 import de.thbingen.vsys.DrPrepper.model.Metadata;
 import de.thbingen.vsys.DrPrepper.model.TransferableFile;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,6 +23,7 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Properties;
 
+@Slf4j
 @SpringBootApplication
 public class DrPrepperApplication {
 
@@ -34,19 +36,26 @@ public class DrPrepperApplication {
 
 	@KafkaListener(topics = "${kafka.topic.file}", containerFactory = "fileKafkaListenerContainerFactory")
 	public void listenWithHeaders(Acknowledgment ack, @Payload String file) {
+	    log.debug("DrPrepper - listenWithHeaders(): Start method for file "+file+".");
         TransferableFile transferableFile = null;
         try {
             transferableFile = new ObjectMapper().readValue(file, TransferableFile.class);
         } catch (JsonProcessingException e) {
+            log.debug("DrPrepper - listenWithHeaders(): Can't create transferable file object for file "+file+".");
             e.printStackTrace();
         }
+        log.debug("DrPrepper - listenWithHeaders(): File "+file+" successfully read.");
         ArrayList<String> dnaSequences = extractDnaSequences(transferableFile);
+        log.debug("DrPrepper - listenWithHeaders(): dna sequences extracted.");
         Metadata metadata = new Metadata(transferableFile);
         // Send sequences
+        log.debug("DrPrepper - listenWithHeaders(): start writing message queue in one transaction.");
         dnaSequenceProducer.sendSequencesInTransaction(metadata,dnaSequences);
+        log.debug("DrPrepper - listenWithHeaders(): finished writing message queue in one transaction.");
         // Commit to files topic
         ack.acknowledge();
-	}
+        log.debug("DrPrepper - listenWithHeaders(): acknowledge file "+file+".");
+    }
 
 	private ArrayList<String> extractDnaSequences(TransferableFile file) {
         ArrayList<String> result = new ArrayList<>();
@@ -57,6 +66,7 @@ public class DrPrepperApplication {
                 }
             }
         } catch (IOException e) {
+            log.debug("DrPrepper - extractDnaSequences(): Failed to read file "+file.getFilePath()+".");
             e.printStackTrace();
         }
         return result;
